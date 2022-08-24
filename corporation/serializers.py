b8fb70f777b685f1e9ffcb75ab088a29e9b54755
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-08-25 00:18:52 +0900

    feat company's other recruitment list added

--- corporation/serializers.py
+++ corporation/serializers.py
@@ -7,23 +7,22 @@ class RecruitmentSerializer(serializers.ModelSerializer):
     company_name = serializers.SerializerMethodField()
     position_name = serializers.SerializerMethodField()
     stack_name = serializers.SerializerMethodField()
-    # country = serializers.SerializerMethodField()
+    related_recruitments= serializers.SerializerMethodField()
+    
     def get_company_name(self,obj):
         return obj.corporation.name
     def get_position_name(self,obj):
         return obj.position.name
     def get_stack_name(self,obj):
         return obj.tech_stack.name
-
-    # def get_country(self,obj):
-    #     obj.corporation.name
-    #     return 
-    
+    def get_related_recruitments(self,obj):
+        recruitments = Recruitment.objects.filter(corporation=obj.corporation.id)
+        return [recruitment.id for recruitment in recruitments]
 
 
     class Meta:
         model = Recruitment
-        fields = ['id', 'company_name', 'position_name', 'country','region','stack_name', 'corporation','position','recompense','content','tech_stack']
+        fields = ['id', 'company_name', 'position_name', 'country','region','stack_name', 'corporation','position','recompense','content','tech_stack', 'related_recruitments']
         extra_kwargs = {
             "corporation": {"write_only": True},
             "position": {"write_only": True},
